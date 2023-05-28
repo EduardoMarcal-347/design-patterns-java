888937b30ae840a55a5769a9aaceaa97b2fd1476
Marçal <edumsfilho347@gmail.com>
2023-05-28 19:44:13 -0300

    criando subsistema responsavel pela aceleraçao

---
+++ facade/facadeDriveVehicle/src/throttleControl.java
@@ -0,0 +1,22 @@
+public class throttleControl {
+
+    private Double speed;
+
+    public throttleControl() {
+        this.speed = 0D;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void speedUp(){
+        speed+=10;
+        System.out.printf("Velocidade Atual: %2.f Km/h", speed);
+    }
+    public void brake(){
+        if (speed>5) speed-=5;
+        else speed-=speed;
+        System.out.printf("Velocidade Atual: %2.f Km/h", speed);
+    }
+}
