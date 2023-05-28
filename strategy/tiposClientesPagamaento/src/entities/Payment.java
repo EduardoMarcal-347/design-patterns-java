4ec5081e89b4a684736bced8114afa99ba177a91
Marçal <edumsfilho347@gmail.com>
2023-05-28 19:16:24 -0300

    finalizando contexto em que uma DiscountStrategy será usada

--- strategy/tiposClientesPagamaento/src/entities/Payment.java
+++ strategy/tiposClientesPagamaento/src/entities/Payment.java
@@ -4,7 +4,11 @@ public class Payment {
 
     private DiscountStrategy discountStrategy;
 
-    
+    public Payment(DiscountStrategy discountStrategy) {
+        this.discountStrategy = discountStrategy;
+    }
 
-
+    public double calculateFinalPrice(Double value){
+        return discountStrategy.discountAplly(value);
+    }
 }
