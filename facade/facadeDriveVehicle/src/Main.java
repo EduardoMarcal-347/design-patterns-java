90428d6c7e202978a38bb2dba3ffdce908858504
Marçal <edumsfilho347@gmail.com>
2023-05-28 20:08:29 -0300

    criando exemplos na classe Main

--- facade/facadeDriveVehicle/src/Main.java
+++ facade/facadeDriveVehicle/src/Main.java
@@ -2,6 +2,19 @@ import entities.DriveMechanism;
 
 public class Main {
     public static void main(String[] args) {
-        DriveMechanism carro = new DriveMechanism()
+        DriveMechanism carro = new DriveMechanism();
+
+        //simulando um carro
+        carro.turnLightsOn();
+        carro.shiftUp();
+        carro.speedUp();
+        carro.speedUp();
+        carro.shiftUp();
+        carro.speedUp();
+        carro.speedUp();
+        carro.brake();
+        carro.brake();
+        carro.shiftDown();
+
     }
 }
