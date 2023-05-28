58329e33b6a1194359d71585e6f2b22352d1e4ec
Marçal <edumsfilho347@gmail.com>
2023-05-28 18:16:20 -0300

    criando instancia e metodo get para o acesso ao atributo privado instance

--- singleton/singletonCalc/src/Calculator.java
+++ singleton/singletonCalc/src/Calculator.java
@@ -1,2 +1,10 @@
 public class Calculator {
+
+    private static Calculator instance;
+
+    public static Calculator getInstance() {
+        if (instance == null) instance = new Calculator();
+        return instance;
+    }
+
 }
