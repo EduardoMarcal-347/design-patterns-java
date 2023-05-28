2cd8cd511d20e053a8942ca5501cc7958c2451b9
Marçal <edumsfilho347@gmail.com>
2023-05-28 19:07:31 -0300

    classe que implementa disconto para clientes com classificação bronze

---
+++ strategy/tiposClientesPagamaento/src/entities/BronzeDiscount.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class BronzeDiscount implements DiscountStrategy {
+
+    @Override
+    public Double discountAplly(Double value) {
+        return value*0.95; //5% de ddesconto
+    }
+    
+}
