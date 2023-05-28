779e5d11b6f2374741ce2c19646f42d41116a8db
Marçal <edumsfilho347@gmail.com>
2023-05-28 19:22:37 -0300

    criando situação de teste

--- strategy/tiposClientesPagamaento/src/Main.java
+++ strategy/tiposClientesPagamaento/src/Main.java
@@ -1,5 +1,20 @@
+import entities.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DiscountStrategy gold = new GoldDiscount();
+        DiscountStrategy silver = new SilverDiscount();
+        DiscountStrategy bronze = new BronzeDiscount();
+        Payment goldClient = new Payment(gold);
+        Payment silverClient = new Payment(silver);
+        Payment bronzeClient = new Payment(bronze);
+
+        // testes usando um produto que teria o valor original de $100
+        System.out.printf("Preço final para cliente com acesso ao gold: $%.2f\n",
+                goldClient.calculateFinalPrice(100D));
+        System.out.printf("Preço final para cliente com acesso ao silver: $%.2f\n",
+                silverClient.calculateFinalPrice(100D));
+        System.out.printf("Preço final para cliente com acesso ao bronze: $%.2f\n",
+                bronzeClient.calculateFinalPrice(100D));
     }
 }
