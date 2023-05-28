ed9ee05ec82aa13cbd3120d98e096761b03be6f2
Marçal <edumsfilho347@gmail.com>
2023-05-28 19:56:01 -0300

    criando subsistema responsavel pelo sistema de farois

--- facade/facadeDriveVehicle/src/HeadlightControl.java
+++ facade/facadeDriveVehicle/src/HeadlightControl.java
@@ -1,2 +1,17 @@
 public class HeadlightControl {
+
+    private boolean enabled;
+
+    public HeadlightControl() {
+        this.enabled = false;
+    }
+
+    public void turnLightsOn() {
+        enabled = true;
+        System.out.println("Os farois estão ligados");
+    }
+    public void turnLightsOff() {
+        enabled = false;
+        System.out.println("Os farois estão desligados");
+    }
 }
