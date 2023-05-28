edb7bca90ca8285180b032e1819e69139f1028d4
Marçal <edumsfilho347@gmail.com>
2023-05-28 18:17:43 -0300

    criando construtor privado, o que impede a criação de instâncias externas.

--- singleton/singletonCalc/src/Calculator.java
+++ singleton/singletonCalc/src/Calculator.java
@@ -2,6 +2,9 @@ public class Calculator {
 
     private static Calculator instance;
 
+    private Calculator() {
+    }
+
     public static Calculator getInstance() {
         if (instance == null) instance = new Calculator();
         return instance;
