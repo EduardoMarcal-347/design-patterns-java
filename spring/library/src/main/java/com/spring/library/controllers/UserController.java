55f9e37b544e79b21aec6ca5b826c92907dd6991
Marçal <edumsfilho347@gmail.com>
2023-05-28 23:19:50 -0300

    criando CRUD basico para UserController

--- spring/library/src/main/java/com/spring/library/controllers/UserController.java
+++ spring/library/src/main/java/com/spring/library/controllers/UserController.java
@@ -1,11 +1,44 @@
 package com.spring.library.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.spring.library.models.User;
+import com.spring.library.services.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/")
 public class UserController {
 
+    private UserService service;
 
+    @GetMapping("users")
+    public List<User> findAllUsers(){
+        return service.findAllUsers();
+    }
+
+    @GetMapping("user/{id}")
+    public Optional<User> findById(@PathVariable("id") Long id){
+        return service.findUserById(id);
+    }
+
+    @PostMapping
+    public User saveUser(@RequestBody User user) {
+        return service.saveUser(user);
+    }
+    @PutMapping("user")
+    public User updateUser(@RequestBody User user) {
+        return service.updateUser(user);
+    }
+    @DeleteMapping("user/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        try {
+            service.deleteUser(id);
+            return "User with ID " + id + " has been deleted!";
+        }catch(Exception e)
+        {
+            return e.getMessage();
+        }
+    }
 }
