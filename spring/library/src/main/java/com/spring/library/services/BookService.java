ef2f54df5adcee9c2c37482013d7876e970d69ac
Marçal <edumsfilho347@gmail.com>
2023-05-28 22:53:57 -0300

    finalindo bookService

---
+++ spring/library/src/main/java/com/spring/library/services/BookService.java
@@ -0,0 +1,48 @@
+package com.spring.library.services;
+
+import com.spring.library.models.Book;
+import com.spring.library.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    @Autowired
+    BookRepository repository;
+
+    public List<Book> findAllBooks(){
+        return repository.findAll();
+    }
+
+    public Optional<Book> findBookById(Long id){
+        var book = repository.findById(id);
+        if(book.isPresent()) return book;
+        else {
+            System.out.println("Livro não encontrado");
+            return null;
+        }
+    }
+
+    public Book saveBook(Book book) {
+        return repository.save(book);
+    }
+    public Book updateBook(Book book) {
+        var dbBook = repository.findById(book.getId());
+        if( dbBook != null && !dbBook.isEmpty()) {
+            return repository.save(book);
+        }
+        return null;
+    }
+
+    public void deleteBook(Long id) {
+        var book = repository.findById(id);
+        if( book != null && !book.isEmpty()) {
+            repository.deleteById(id);
+        }
+    }
+
+}
