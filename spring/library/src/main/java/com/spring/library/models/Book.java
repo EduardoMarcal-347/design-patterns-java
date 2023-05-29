3be38e11c0854d99f56b859fa358182b74ad2c42
Marçal <edumsfilho347@gmail.com>
2023-05-28 22:21:51 -0300

    finalizando classe Book

--- spring/library/src/main/java/com/spring/library/models/Book.java
+++ spring/library/src/main/java/com/spring/library/models/Book.java
@@ -2,6 +2,8 @@ package com.spring.library.models;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Table(name = "book_table")
 public class Book {
 
@@ -18,5 +20,66 @@ public class Book {
     @Column(name = "user")
     private User user;
 
+    public Book() {
+    }
 
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", user=" + user +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author);
+    }
 }
