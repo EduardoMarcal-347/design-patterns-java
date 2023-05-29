fee07d7e12b91b1c174c286030f08357220c7c9b
Marçal <edumsfilho347@gmail.com>
2023-05-28 22:05:30 -0300

    configurando atributos e construtor do model User

--- spring/library/src/main/java/com/spring/library/models/User.java
+++ spring/library/src/main/java/com/spring/library/models/User.java
@@ -1,15 +1,31 @@
 package com.spring.library.models;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 
 @Table(name = "user_table")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "user_name", length = 100, nullable = false)
     private String name;
+    @Column(name = "user_email", length = 75, nullable = false)
     private String email;
+    @Column(name = "user_phone", length = 25, nullable = false)
     private String phone;
 
+    public User() {
+    }
+
+    public User(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    
 }
